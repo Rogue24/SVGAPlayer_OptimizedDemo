73b02bca7a4a6095585d992033ae8ca76fb9d0c5
zhoujianping <zhoujianping24@hotmail.com>
2023-11-20 18:00:53 +0800

    添加新文件

---
+++ SVGAParsePlayer_Demo/SVGAParsePlayer/SVGAVideoEntity+Extension.h
@@ -0,0 +1,16 @@
+//
+//  SVGAVideoEntity+Extension.h
+//  SVGAParsePlayer_Demo
+//
+//  Created by aa on 2023/11/20.
+//
+
+#import <SVGAPlayer/SVGAPlayer.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SVGAVideoEntity (Extension)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SVGAParsePlayer_Demo/SVGAParsePlayer/SVGAVideoEntity+Extension.m
@@ -0,0 +1,12 @@
+//
+//  SVGAVideoEntity+Extension.m
+//  SVGAParsePlayer_Demo
+//
+//  Created by aa on 2023/11/20.
+//
+
+#import "SVGAVideoEntity+Extension.h"
+
+@implementation SVGAVideoEntity (Extension)
+
+@end
